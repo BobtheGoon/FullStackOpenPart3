d2904e0dfb0cba399a942232a1a7e80132428868
Teemu <teemu9598@gmail.com>
2022-08-28 14:58:22 +0300

    Finish 3.5

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+
 let persons = [
     {
         id: 1,
@@ -26,38 +28,52 @@ let persons = [
 ];
 
 
+const generateID = () => {
+    return Math.floor(Math.random() * 10000);
+}
+
+
 app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
 
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
+    const id = Number(req.params.id);
+    const person = persons.find(p => p.id === id);
     if (person) {
-        res.json(person)
+        res.json(person);
         }
     else {
-        res.status(404).end()
+        res.status(404).end();
     }
 })
 
 
 app.get('/info', (req, res) => {
-    date = new Date()
+    date = new Date();
     res.send(
         `<div>Phonebook has info for ${persons.length} people</div>
         <div>${date}</div>`
-    )
+    );
 })
 
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
+    const id = Number(req.params.id);
+    persons = persons.filter(p => p.id !== id);
   
-    res.status(204).end()
+    res.status(204).end();
   })
+
+
+app.post('/api/persons', (req, res) => {
+    const person = req.body;
+    const id = generateID();
+    person.id = id;
+    persons = persons.concat(person);
+    res.json(person);
+})
 
 
 const PORT = 3001;
