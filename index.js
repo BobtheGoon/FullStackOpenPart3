6181c3575a8e759663a9eb8f063ddc8d368c20d4
Teemu <teemu9598@gmail.com>
2022-09-05 21:53:07 +0300

    Finish 3.16-3.18

--- index.js
+++ index.js
@@ -15,11 +15,6 @@ app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 
-const generateID = () => {
-    return Math.floor(Math.random() * 10000);
-};
-
-
 app.get('/api/persons', (req, res) => {
     Person.find({}).then(persons => {
         res.json(persons)
@@ -27,9 +22,8 @@ app.get('/api/persons', (req, res) => {
 });
 
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     const id = req.params.id;
-    console.log(id)
 
     Person.findById(id).then(person => {
     
@@ -39,7 +33,8 @@ app.get('/api/persons/:id', (req, res) => {
         else {
             res.status(404).end();
             }
-    });
+        })
+        .catch(error => next(error))
 });
 
 
@@ -56,17 +51,7 @@ app.get('/info', (req, res) => {
 });
 
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = req.params.id;
-    Person.findByIdAndRemove(id)
-        .then(result => {
-            res.status(204).end()
-        });
-  });
-
-
-app.post('/api/persons', (req, res) => {
-    const body = req.body
+app.post('/api/persons', (req, res, next) => {
     //Check if name already exists in phonebook
     // let exists = false;
 
@@ -75,41 +60,82 @@ app.post('/api/persons', (req, res) => {
     //         exists = true;
     //     };
     // })
+    //if (exists) {
+    //     return res.status(409).json({
+    //         error: 'name already exists'
+    //     });
+    // }
 
     //Check if request contains valid information
-    
-    if (!body.name) {
+    if (!req.body.name) {
         return res.status(400).json({
             error: 'missing name'
         });
     }
 
-    else if (!body.number) {
+    if (!req.body.number) {
         return res.status(400).json({
             error: 'missing number'
         });
     }
 
-    // else if (exists) {
-    //     return res.status(409).json({
-    //         error: 'name already exists'
-    //     });
-    // }
-    
     const person = new Person({
-        name: body.name,
-        number: body.number,
+        name: req.body.name,
+        number: req.body.number,
         date: new Date(),
-    })
+        })
 
     person.save().then(savedPerson => {
         res.json(savedPerson);
-    });
+        })
+        .catch(error => next(error))
 });
+
+
+app.delete('/api/persons/:id', (req, res, next) => {
+    const id = req.params.id;
+    Person.findByIdAndRemove(id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
+  });
+
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const person = {
+        name: req.body.name,
+        number: req.body.number,
+    };
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
+});
+
+
+//Handle cast errors
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message);
+    console.log(error.name)
+
+    if (error.name === 'CastError') {
+        return res.status(400).send({error: 'malformatted id'})
+        }
+
+    else if (error.name === 'TypeError') {
+        return res.status(400).send({error: 'person already removed'})
+        };
+
+    next(error);
+    }
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
-
